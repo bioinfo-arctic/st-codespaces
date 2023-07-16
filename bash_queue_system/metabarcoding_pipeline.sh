c51b5d357047bb2e534574ed243167d2b48aa4ae
bioinfo-arctic <126463272+bioinfo-arctic@users.noreply.github.com>
2023-07-16 19:23:54 +0000

    adding the directory and creating the queue file.

--- bash_queue_system/metabarcoding_pipeline.sh
+++ bash_queue_system/metabarcoding_pipeline.sh
@@ -1,7 +1,22 @@
-# first step, check if there is any metabarcoding dataset currently running
+# This script will process your metabarcoding files using a queue system.
+# It has to be called as below:
+# /workspaces/st-codespaces/bash_queue_system/metabarcoding_pipeline.sh your_file_address
+
+# Here is the step-by-step
+# Check if the queue_manager folder exists, if not, create one.
+# The "-p" parameter will check if the folder already exists. If so it won't create a new one and won't through an error.
+mkdir -p ~/queue_manager
+
+# For making sure there is a queue, run this command:
+touch ~/queue_manager/metabarcoding_analyses_queue.txt
+
+# Check if there is any metabarcoding dataset currently running
 if [ -f ~/queue_manager/running_file.txt ];
 then
-  echo -e "# $1 will be next \n$1"
+  echo -e "# $1 will be added to the queue \n"
+  N_OF_FILES_TO_RUN=`wc -l ~/queue_manager/metabarcoding_analyses_queue.txt | cut -f 1 -d " "`
+  echo -e "There are ${N_OF_FILES_TO_RUN} files in the queue\n"
+  echo "/workspaces/st-codespaces/bash_queue_system/metabarcoding_pipeline.sh /new_user_00/his_file.tar.gz" >> ~/queue_manager/metabarcoding_analyses_queue.txt
 else
   touch ~/queue_manager/running_file.txt
   echo -e "running $1\n" > ~/queue_manager/running_file.txt
