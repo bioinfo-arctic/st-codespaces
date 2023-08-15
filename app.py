61b1657145000fcc0699cd93de38d23f86e70492
HenriChap <92765975+HenriChap@users.noreply.github.com>
2023-08-15 19:06:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import yaml
 from yaml.loader import SafeLoader
 
 st.set_page_config("AutoReport")
-with open('../config.yaml') as file:
+with open('./hash_config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = st_auth.Authenticate(
