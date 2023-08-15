e52c8eac39a4c765546ccb3b8bbb0ef1f4ed44ba
HenriChap <92765975+HenriChap@users.noreply.github.com>
2023-08-15 19:04:31 +0000

    Update keys.py

--- keys.py
+++ keys.py
@@ -5,8 +5,10 @@ import streamlit_authenticator as st_auth
 
 with open('./config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-    hashed_passwords = st_auth.Hasher([config['credentials']['usernames']['jd94']['password'],config['credentials']['usernames']['princess_doe']['password']]).generate()
-    config['credentials']['usernames']['jd94']['hashed_password'] = hashed_passwords[0]
-    config['credentials']['usernames']['princess_doe']['hashed_password'] = hashed_passwords[1]
+    for i in config['credentials']['usernames']:
+        for j in config['credentials']['usernames'][i]:
+            if j == 'password':
+                config['credentials']['usernames'][i]['password'] = st_auth.Hasher([config['credentials']['usernames'][i][j]]).generate()
+
 with open('./hash_config.yaml', 'w') as dump_file:
     yaml.dump(config, dump_file)
